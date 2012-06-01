d2a9c19f54b46377cf07ea35bd08f82441bb1885
Jacob Peck <suschord@suspended-chord.info>
2012-05-31 22:33:13 -0400

    Look for error pages in templates/pages/<code>.rhtml

--- lib/toto.rb
+++ lib/toto.rb
@@ -300,7 +300,7 @@ module Toto
         ERB.new(File.read("#{path}/#{page}.rhtml")).result(ctx)
       },
       :error => lambda {|code, ctx|                              # The HTML for your error page
-        ERB.new(File.read("#{Paths[:pages]}/#{code}.rhtml")).result(&Proc.new { content })
+        ERB.new(File.read("#{Paths[:pages]}/#{code}.rhtml")).result(&Proc.new { to_html page, @config })
       }
     }
     def initialize obj
